a428e8ce85b9544815b015af96b80e770c834c1e
Tim Austin <tim@neenjaw.com>
2020-05-28 19:01:15 -0600

    starting point

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
-'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const app = express();
 
-var cors = require('cors');
+// Basic Configuration
+const port = process.env.PORT;
 
-var app = express();
+const mongoose = require('mongoose')
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+const mongoDbUser = process.env.MONGODB_USER
+const mongoDbPw = process.env.MONGODB_PW
+const mongoDbName = process.env.MONGODB_NAME
+const mongoUri = `mongodb+srv://${mongoDbUser}:${mongoDbPw}@${mongoDbName}`
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}))
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
+// Serve the public directory
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// Serve the index to the root route
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
