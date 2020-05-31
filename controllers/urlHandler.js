d56a0a8806ec964917f130014d3823cdd0c46cae
Tim Austin <tim@neenjaw.com>
2020-05-31 15:28:12 -0600

    promisify

--- controllers/urlHandler.js
+++ controllers/urlHandler.js
@@ -1,55 +1,70 @@
-const dns = require('dns')
+const dns = require("dns");
 
-const { Counter } = require('../models/counter')
-const { Url } = require('../models/url')
+const { Counter } = require("../models/counter");
+const { Url } = require("../models/url");
 
 const getShortUrl = (req, res) => {
-  const shortUrl = req.params.shortUrl
+  const shortUrl = req.params.shortUrl;
 
-  Url.findOne({shortUrl}, (err, url) => {
+  Url.findOne({ shortUrl }, (err, url) => {
     if (err) {
-      return err
+      return err;
     }
 
-    console.log(url)
-    res.redirect(`https://${url.url}`)
-  })
-}
+    console.log(url);
+    res.redirect(`https://${url.url}`);
+  });
+};
+
+const lookupPromise = (url) =>
+  new Promise((resolve, reject) => {
+    dns.lookup(url, (err, address, family) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve([address, family]);
+      }
+    });
+  });
 
 const createShortUrl = (req, res) => {
-  const postedUrl = req.body.url
-  const urlRegex = /^(https?:\/\/)?(?<url>.*)$/
-  const {groups: {url: bareUrl}} = urlRegex.exec(postedUrl)
+  const postedUrl = req.body.url;
+  const urlRegex = /^(https?:\/\/)?(?<url>.*)$/;
+  const {
+    groups: { url: bareUrl },
+  } = urlRegex.exec(postedUrl);
 
-  dns.lookup(bareUrl, (err, address) => {
-    if (err) {
-      return res.json({"error":"invalid URL"})
-    }
-
-    const counterQuery = {}
-    const counterUpdate = {$inc: {count: 1}}
-    const counterOptions = {upsert: true, new: true, setDefaultsOnInsert: true}
-    Counter.findOneAndUpdate(counterQuery, counterUpdate, counterOptions, (err, counter) => {
-      if (err) return err
-
-      const newUrl = new Url({
-        url: bareUrl,
-        shortUrl: counter.count
-      })
-
-      newUrl.save((err, savedUrl) => {
-        if (err) return err
-
-        res.json({
-          original_url: savedUrl.url,
-          short_url: savedUrl.shortUrl
+  lookupPromise(bareUrl)
+    .then(([address, _]) => {
+      Counter.findOneAndUpdate(
+        {},
+        { $inc: { count: 1 } },
+        { upsert: true, new: true, setDefaultsOnInsert: true }
+      )
+        .then((counter) => {
+          new Url({
+            url: bareUrl,
+            shortUrl: counter.count,
+          })
+            .save()
+            .then((savedUrl) =>
+              res.json({
+                original_url: savedUrl.url,
+                short_url: savedUrl.shortUrl,
+              })
+            )
+            .catch((err) => {
+              res.json({ error: "unable to save shortened URL" });
+            });
         })
-      })
+        .catch((err) =>
+          res.json({ error: "unable to obtain new shortened URL" })
+        );
     })
-  })
-}
+    .catch((err) => res.json({ error: "invalid URL" }));
+};
 
 module.exports = {
   getShortUrl,
-  createShortUrl
-}
+  createShortUrl,
+};
